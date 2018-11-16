27783f2a2a50af5acb1f1fd4ba09c32871fbbd03
burt <burt.wang@dongmancorp.cn>
2018-11-16 22:15:59 +0800

    commit

--- src/main/java/com/burt/GrayscaleService1Application.java
+++ src/main/java/com/burt/GrayscaleService1Application.java
@@ -4,14 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableZuulProxy
 @EnableEurekaClient
-@EnableFeignClients
 public class GrayscaleService1Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
